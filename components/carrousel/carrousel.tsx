b69188ef9b1344e808b2007db53420549d14eb35
poomcha <poompoomcha@gmail.com>
2023-12-05 18:13:56 +0100

    Ajout de dots de translation images portfolio.

--- components/carrousel/carrousel.tsx
+++ components/carrousel/carrousel.tsx
@@ -4,7 +4,7 @@ import styles from './carrousel.module.css';
 
 import { CarrouselPropsI } from '@/app/portfolio/page';
 import { InstagramMediaI } from '@/context/instagram';
-import { Dispatch, useEffect, useState } from 'react';
+import { Dispatch, MouseEvent, useEffect, useState } from 'react';
 import Image from 'next/image';
 
 export default function Carrousel(props: CarrouselPropsI) {
@@ -12,6 +12,8 @@ export default function Carrousel(props: CarrouselPropsI) {
     undefined | InstagramMediaI[],
     Dispatch<InstagramMediaI[] | undefined>
   ];
+
+  const [activeId, setActiveId] = useState('');
 
   const getCarouselDatas = async (props: CarrouselPropsI) => {
     const server_url = process.env.NODE_ENV ? '' : 'http://localhost:3000';
@@ -31,8 +33,12 @@ export default function Carrousel(props: CarrouselPropsI) {
 
   const computed_images =
     props.is_album && images ? (
-      images.map((image) => (
-        <div className={styles.image_ctn} key={image.id}>
+      images.map((image, index) => (
+        <div
+          className={styles.image_ctn}
+          key={image.id}
+          id={`${image.id}-${index}`}
+        >
           <Image
             src={image.media_url}
             alt={"Image d'album @blandinemakeup34."}
@@ -56,5 +62,43 @@ export default function Carrousel(props: CarrouselPropsI) {
       </div>
     );
 
-  return <div className={styles.carrousel}>{computed_images}</div>;
+  const handleScrollIntoView = (e: MouseEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+    const target = document.getElementById(e.target.dataset.id);
+    target?.scrollIntoView({
+      behavior: 'smooth',
+      block: 'center',
+      inline: 'start',
+    });
+    setActiveId(e.target.dataset.id);
+  };
+
+  let link_dots;
+  if (Array.isArray(computed_images) && images) {
+    link_dots = images.map((image, index, array) => (
+      <button
+        className={styles.linked_dot}
+        key={`${image.id}-${index}`}
+        data-id={`${image.id}-${index}`}
+        data-activeid={activeId ? activeId : `${array[0].id}-${0}`}
+        data-isactive={
+          activeId &&
+          images.find((image) => `${image.id}` === activeId.split('-')[0])
+            ? `${image.id}-${index}` === activeId
+            : `${array[0].id}-${0}` === `${image.id}-${index}`
+        }
+        onClick={handleScrollIntoView}
+      ></button>
+    ));
+  }
+
+  return (
+    <div className={styles.carrousel}>
+      {computed_images}
+      {Array.isArray(computed_images) && images && (
+        <div className={styles.linked_dot_ctn}>{link_dots}</div>
+      )}
+    </div>
+  );
 }
