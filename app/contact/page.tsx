ad400bea83934abd1d0bf781cf52c6539d85880e
poomcha <poompoomcha@gmail.com>
2024-01-28 02:28:18 +0100

    Test màj font-size

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,19 +1,19 @@
-'use client';
+"use client";
 
-import { InstagramContext, InstagramI } from '@/context/instagram';
-import { useContext, Context } from 'react';
-import { get_media_by_hashtag } from '../utils/instagram';
-import styles from './contact.module.css';
-import cN from 'classnames';
-import Image from 'next/image';
-import MailForm from '@/components/mailform/mailform';
-import Social from '@/components/social/social';
+import { InstagramContext, InstagramI } from "@/context/instagram";
+import { useContext, Context } from "react";
+import { get_media_by_hashtag } from "../utils/instagram";
+import styles from "./contact.module.css";
+import cN from "classnames";
+import Image from "next/image";
+import MailForm from "@/components/mailform/mailform";
+import Social from "@/components/social/social";
 
 export default function Contact() {
   const instagram = useContext(InstagramContext as Context<InstagramI>);
-  const media_url = get_media_by_hashtag(instagram, 'contact');
+  const media_url = get_media_by_hashtag(instagram, "contact");
   return (
-    <div className={cN(styles.contact, 'page')}>
+    <div className={cN(styles.contact, "page")}>
       <div className={styles.imgctn}>
         {media_url && (
           <Image
